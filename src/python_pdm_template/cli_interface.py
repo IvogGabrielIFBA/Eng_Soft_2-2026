76aca43a4d8cd34b9e8e384e725cd6ce75d997bc
Thiago Santos Guimarães <thiago19guimaraes@gmail.com>
2026-05-10 22:12:50 -0300

    test: elaboração dos testes unitários (TDD First) e moldes das funcionalidades

---
+++ src/python_pdm_template/cli_interface.py
@@ -0,0 +1,7 @@
+"""
+Interface de Linha de Comando (CLI).
+
+Este módulo define a estrutura de comandos, flags e parâmetros 
+utilizando a biblioteca Typer. É responsável por traduzir as 
+entradas do terminal em ações para o Core do sistema (RF002).
+"""
---
+++ src/python_pdm_template/gui_interface.py
@@ -0,0 +1,6 @@
+"""
+Interface Gráfica de Usuário (GUI).
+
+Este módulo implementa a interface visual baseada em PySide6, 
+utilizando o padrão de navegação Single Page Application (SPA) (RF003).
+"""
